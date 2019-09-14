23fa18baff67b098cbc82e37d7f62cafbf90d308
BuildTools <colorlord98@gmail.com>
2019-09-14 13:05:23 +0300

    Moved MouseButton enum to an Enums.h

---
+++ src/Enums.h
@@ -0,0 +1,10 @@
+#pragma once
+
+enum class MouseButton {
+	None 			= 0,
+	MouseButton1 	= 1 << 0,
+	MouseButton2 	= 1 << 1,
+	MouseButton3 	= 1 << 2,
+	MouseButton4 	= 1 << 3,
+	MouseButton5	= 1 << 4
+};
