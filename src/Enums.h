771ede2a1037cc1a89c10a91ba9d693528823a8f
BuildTools <colorlord98@gmail.com>
2019-09-14 13:22:02 +0300

    Changed MouseButton enum to use incrementing values instead of power 2 values

--- src/Enums.h
+++ src/Enums.h
@@ -1,10 +1,10 @@
 #pragma once
 
 enum class MouseButton {
-	None 			= 0,
-	MouseButton1 	= 1 << 0,
-	MouseButton2 	= 1 << 1,
-	MouseButton3 	= 1 << 2,
-	MouseButton4 	= 1 << 3,
-	MouseButton5	= 1 << 4
+	None = 0,
+	MouseButton1,
+	MouseButton2,
+	MouseButton3,
+	MouseButton4,
+	MouseButton5
 };
