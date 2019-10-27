12fdf93b53f24e143b3ef39bd899cfb898c15b4c
rares <colorlord98@gmail.com>
2019-10-27 14:29:59 +0200

    Tablet driver now uses FuckMake

--- compile.sh
+++ compile.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-g++ src/CursorHelper.cpp src/TabletDriver.cpp src/Area.cpp src/main.cpp -lX11 -lXtst `pkg-config --libs --cflags libusb-1.0` -g -o driver.o
+if [ ! -d tools ]; then
+	echo "Creating tools directory"
+	$(mkdir tools)
+fi
+
+if [ ! -f tools/FuckMake ]; then
+	echo "Downloading FuckMake"
+	$(curl -L -o "tools/FuckMake" "https://github.com/JeppeSRC/FuckMake/releases/download/V1.1/FuckMake")
+	$(chmod +x tools/FuckMake)
+fi
+
+if [ -f driver ]; then
+	rm driver
+fi
+
+./tools/FuckMake
