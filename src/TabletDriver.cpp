add16287806c91ffe617a88e4c6ed6edc048bf9e
rares <colorlord98@gmail.com>
2019-10-27 11:09:04 +0200

    Replaced spaces with tabs -.-

--- src/TabletDriver.cpp
+++ src/TabletDriver.cpp
@@ -2,49 +2,49 @@
 
 
 TabletDriver::TabletDriver(const char* configFolder) {
-    libusb_init(&m_Context);
-    printf("Getting the device list...\n");
+	libusb_init(&m_Context);
+	printf("Getting the device list...\n");
 	m_ListSize = libusb_get_device_list(m_Context, &m_List); 	
 	printf("Number of devices: %lu\n", m_ListSize);
 
-    m_FoundTablet = findTablet();
-    if (!m_FoundTablet) {
-        printf("Could not find tablet.");
-        return;
-    }
+	m_FoundTablet = findTablet();
+	if (!m_FoundTablet) {
+		printf("Could not find tablet.");
+		return;
+	}
 	else {
 		printf("Found tablet!\n");
 	}
 
-    if (libusb_kernel_driver_active(m_TabletHandle, 0) != 0) {
+	if (libusb_kernel_driver_active(m_TabletHandle, 0) != 0) {
 		printf("Kernel driver is active, detaching...\n");
 		int res = libusb_detach_kernel_driver(m_TabletHandle, 0);
 		if (res != 0)
-            return;
+			return;
 		printf("Kernel driver detached!\n");
 	}
 }
 
 TabletDriver::~TabletDriver() {
-    libusb_release_interface(m_TabletHandle, 0);
+	libusb_release_interface(m_TabletHandle, 0);
 	libusb_close(m_TabletHandle);
 	libusb_free_device_list(m_List, 1);
 	libusb_exit(NULL);
 }
 
 TabletPacket TabletDriver::getData() {
-    int transferred;
-    int res = libusb_interrupt_transfer(m_TabletHandle, 0x82, buffer, sizeof(buffer), &transferred, 10);
-    if (transferred)
-        return TabletPacket(buffer, 0);
-    return TabletPacket();
+	int transferred;
+	int res = libusb_interrupt_transfer(m_TabletHandle, 0x82, buffer, sizeof(buffer), &transferred, 10);
+	if (transferred)
+		return TabletPacket(buffer, 0);
+	return TabletPacket();
 }
 
 bool TabletDriver::findTablet() {
-    // hardcoded name for now
-    const char* name = "G430S";
+	// hardcoded name for now
+	const char* name = "G430S";
 
-    int res = 0;
+	int res = 0;
 	unsigned char uname[250] = {0};
 	for (size_t i = 0; i <= strlen(name); i++)
 		uname[i] = static_cast<unsigned char>(name[i]);
@@ -60,7 +60,7 @@ bool TabletDriver::findTablet() {
 		res = libusb_get_string_descriptor_ascii(deviceHandle, desc.iProduct, productName, sizeof(productName));
 		printf("Found device, checking if it's the correct name...\n");
 		if (memcmp(productName, uname, strlen(name)) == 0) {
-            m_TabletHandle = deviceHandle;
+			m_TabletHandle = deviceHandle;
 			return true;
 		}
 		libusb_close(deviceHandle);
